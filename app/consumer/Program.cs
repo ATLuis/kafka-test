82f80d5f64a99e88f4fe46a9df8a9a8a824c3b00
Lc <ltarazona@outlook.com>
2024-11-25 17:30:44 +0000

    make kafka messages dynamic

---
+++ app/consumer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Confluent.Kafka;
+
+namespace consumer
+{
+    class Program
+    {
+        static readonly string TOPIC = "test-topic";
+        static readonly string GROUP_ID = "my-consumer-group";
+        static void Main(string[] args)
+        {
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = "kafka:29092",
+                GroupId = GROUP_ID,
+                AutoOffsetReset = AutoOffsetReset.Earliest,
+                // Debug = "all"
+            };
+
+            using var consumer = new ConsumerBuilder<Ignore, string>(config)
+                .SetPartitionsAssignedHandler((c, partitions) =>
+                {
+                    Console.WriteLine($"Consumer started for group id {GROUP_ID} assigned to partition {string.Join(", ", partitions)}");
+                })
+                .SetPartitionsRevokedHandler((c, partitions) =>
+                {
+                    Console.WriteLine($"Consumer revoked for group id {GROUP_ID} and topic {string.Join(", ", partitions)}");
+                })
+                .Build();
+
+            consumer.Subscribe(TOPIC);
+
+            Console.WriteLine($"Consumer starting for group id {GROUP_ID} and topic {TOPIC}");
+
+            while (true)
+            {
+                var consumeResult = consumer.Consume();
+                Console.WriteLine($"Received message: {consumeResult.Message.Value}");
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ app/producer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Confluent.Kafka;
+
+namespace producer
+{
+    class Program
+    {
+        static readonly string TOPIC = "test-topic";
+        static void Main(string[] args)
+        {
+            var config = new ProducerConfig { BootstrapServers = "kafka:29092" };
+
+            using var producer = new ProducerBuilder<Null, string>(config).Build();
+
+            while (true)
+            {
+                Console.WriteLine("Enter your message (type 'exit' to quit):");
+                var input = Console.ReadLine();
+                if (input.ToLower() == "exit")
+                {
+                    break;
+                }
+
+                producer.Produce(TOPIC, new Message<Null, string> { Value = input });
+                producer.Flush();
+                Console.WriteLine("Message sent!");
+            }
+        }
+    }
+}
